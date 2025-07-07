e68f57e6cd261a7a35a18fcc2ad99174b25565ee
Vinu Prabhakaran <vinu.cp@gmail.com>
2025-07-06 22:49:15 -0400

    Addition to Basics

--- LearnPython/00_Basics.py
+++ LearnPython/00_Basics.py
@@ -13,3 +13,15 @@ if __name__ == '__main__':
     myDict = {'k1':1,'k2':2,'k3':3}
     for key,value in myDict.items():
         print(f'{key} : {value}')
+
+    # While block repeatedly asking for number
+    while True:
+        try:
+            user_input = int(input("Enter a number"))
+        except:
+            print('Not a number!')
+        else:
+            print('Valid input received')
+            break
+        finally:
+            print('I will always run at the end')
